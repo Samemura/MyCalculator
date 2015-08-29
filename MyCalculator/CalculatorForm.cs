c7513c5faa2d2bda53a6ab20e2de2a1c2da1790e
Samemura <mrnmsk.0range.m2a@gmail.com>
2015-08-29 17:37:54 +0900

    Basic implementation done.

---
+++ MyCalculator/CalculatorForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MyCalculator
+{
+    public partial class CalculatorForm : Form
+    {
+        private CalculatorModel calcModel;
+        private string inputNumText;
+
+        public CalculatorForm()
+        {
+            InitializeComponent();
+            calcModel = new CalculatorModel();
+            inputNumText = "";
+            ResultTextBox.Text = "0";
+        }
+
+        private void NumButton_Click(object sender, EventArgs e)
+        {
+            if (inputNumText == "0")
+                inputNumText = "";
+            inputNumText += ((Button)sender).Text;
+            ResultTextBox.Text = inputNumText;
+        }
+
+        private void OpeButton_Click(object sender, EventArgs e)
+        {
+            string input = ((Button)sender).Text;
+            calcModel.inputOperator(input, double.Parse(ResultTextBox.Text));
+            inputNumText = "";
+            ResultTextBox.Text = calcModel.result.ToString();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            calcModel.clearResult();
+            inputNumText = "";
+            ResultTextBox.Text = "0";
+        }
+    }
+}
---
+++ MyCalculator/CalculatorModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyCalculator
+{
+    class CalculatorModel
+    {
+        public double result;
+        private string preOperator;
+
+        public CalculatorModel()
+        {
+            clearResult();
+        }
+
+        public void inputOperator(String inputOperator, double inputNum)
+        {
+            switch (preOperator)
+            {
+                case "+":
+                    result += inputNum;
+                    break;
+                case "-":
+                    result -= inputNum;
+                    break;
+                case "*":
+                    result *= inputNum;
+                    break;
+                case "/":
+                    result /= inputNum;
+                    break;
+                default:
+                    result = inputNum;
+                    break;
+            }
+            preOperator = inputOperator;
+        }
+
+        public void clearResult()
+        {
+            result = 0;
+            preOperator = "";
+        }
+
+    }
+}
